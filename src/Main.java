1a2f5d5375a8aab39afd1e2961698f21721e63fa
idrisimo <idrissilva@hotmail.com>
2022-08-18 17:32:30 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // primitive data types
+        int hello_world = 5;
+        double num2 = 5.0;
+        boolean b = true;
+        char c = 'h';
+
+        // not primitive
+        String str = "idris";
+
+
+        System.out.println(str);
+    }
+}
