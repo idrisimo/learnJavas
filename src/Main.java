83021fa648687777d842d57abc2f301ca9fac3fd
idrisimo <idrissilva@hotmail.com>
2022-08-19 10:18:09 +0100

    arrays

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // primitive data types
-        int hello_world = 5;
-        double num2 = 5.0;
-        boolean b = true;
-        char c = 'h';
 
-        // not primitive
-        String str = "idris";
-
-        System.out.println(str);
+        int[] newArr = new int[3];
+        newArr[0] = 1;
+        newArr[1] = 2;
+        newArr[2] = 3;
         
-        int x = 5;
-        int y = 7;
-        int z = 56;
-        double sum = x*y/(double)z;
-        double d = Math.pow(x,y);
+        int x = newArr[0];
+        System.out.println(x);
 
-        System.out.println(sum);
-
-        Scanner sc = new Scanner(System.in);
-        String scanned = sc.next();
-        // int scanned = sc.nextInt();
-        // boolean scanned = sc.nextBoolean();
-        int num3 = Integer.parseInt(scanned);
-
-        System.out.println(scanned);
-        System.out.println(num3);
+        int[] nums = {1,2,3};
+        System.out.println(nums[1]);
+        
 
     }
 }
